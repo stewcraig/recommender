bd6a8027d626f65248aa36bb6262df16c33c9ca4
Andrew <apc53c@gmail.com>
2017-08-20 19:32:16 +0100

    Initial commit.

---
+++ manage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def static_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)

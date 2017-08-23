ccb650a97e3ad2d81bd2dcc339ba12180dbb96cd
Andrew <apc53c@gmail.com>
2017-08-23 20:23:27 +0100

    Now gets jQuery source from Google API. Sets $SCRIPT_ROOT. Should respond to 'search'.

--- manage.py
+++ manage.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
 def static_page():
     return render_template('index.html')
+
+@app.route('/search')
+def test_reply():
+	value = str(request.args.get('value', "DEFAULT VALUE"))
+	return_value = value.upper()
+    return jsonify(result = "JS said: " + value + " and Flask replied " + return_value)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)

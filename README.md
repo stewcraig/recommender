65556545ec8fbceb5ac945019eacb0335ae50db8
Andrew <apc53c@gmail.com>
2017-08-27 11:40:34 +0100

    Added README.md. Put GET processing in try/except.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Heroku Flask/JavaScript template
+
+A template for a Heroku app with a Python Flask backend and a JavaScript frontend.
+
+To use this to create a Heroku app, you will need to have Heroku installed ([instructions here](https://devcenter.heroku.com/articles/heroku-cli)).
+
+1. Clone this repo into a directory.
+
+2. In that directory, create the app:
+```
+heroku create NAME_OF_APP
+```
+
+3. Push to the app with:
+```
+git push heroku master
+```
+
+The app should now be running on Heroku.
--- manage.py
+++ manage.py
@@ -9,9 +9,12 @@ def static_page():
 
 @app.route('/search/', methods=['GET'])
 def test_reply():
-	value = str(request.args.get('value', "DEFAULT VALUE"))
-	return_value = value.upper()
-	return jsonify(result = "JS said: " + value + " and Flask replied " + return_value)
+	try:
+		value = request.args.get('value', "DEFAULT VALUE", type=str)
+		return_value = value.upper()
+		return jsonify(result = "JS said: " + value + " and Flask replied: " + return_value)
+	except Error as e:
+		print(e)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)

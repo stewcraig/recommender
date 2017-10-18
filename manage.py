54482a5d5149ba49d9d5cbeda3d83bb5d0da5a30
Andrew <apc53c@gmail.com>
2017-10-18 21:49:18 +0100

    In process of adding recommender.

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@
 from flask import Flask, render_template, jsonify, request
 import os, psycopg2
 from urllib import parse
+import pandas as pd
+from recommender_m1 import MovieLensRecommender
 
 # Get database connection.
 parse.uses_netloc.append("postgres")
@@ -10,9 +12,31 @@ url = parse.urlparse(os.environ["DATABASE_URL"])
 conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
 cur = conn.cursor()
 # Try to get something from database.
-cur.execute("""SELECT title from movies""")
+movies_df = pd.DataFrame(columns=["movieid", "movierow", "title", "year", "genre"])
+cur.execute("""SELECT movieid, movierow, title, year, genre from movies""")
 movie_rows = cur.fetchall()
-		
+for row in movie_rows:
+	factorised_movies = factorised_movies.append(row[0:5])
+
+factorised_movies = pd.DataFrame(columns=["movieid", "movierow", "title", "year", "genre"])
+cur.execute("""SELECT movieid, movierow, title, year, genre from movies""")
+movie_rows = cur.fetchall()
+for row in movie_rows:
+	factorised_movies = factorised_movies.append(row[0:5])
+
+factorised_diag = pd.DataFrame(columns=["diag"])
+cur.execute("""SELECT * from diag""")
+diag_rows = cur.fetchall()
+for row in diag_rows:
+	factorised_diag = factorised_diag.append(row[0])
+
+# Set up recommender.
+m = MovieLensRecommender(factorised_movies=factorised_movies,
+                         factorised_diag=factorised_diag,
+                         movie_df=movies_df)
+new_user_ratings = {'Die Hard':5, 'Terminator':5, 'Speed':5}
+predictions_df = m.get_predictions(new_user_ratings)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,8 +47,12 @@ def static_page():
 def test_reply():
 	try:
 		value = request.args.get('value', "DEFAULT VALUE", type=str)
-		return_value = value.upper() + " and movie title: " + str(movie_rows[0])
-		return jsonify(result = "JS said: " + value + " and Flask replied: " + return_value)
+		return_value = value.upper()
+		predictions_string = predictions_df.sort_values('prediction', ascending=False).head()
+		return jsonify(result = "JS said: " + value + " and Flask replied: " + return_value +
+				"First row of movies table: " + str(movie_rows[0]) +
+				". New ratings: " + str(new_user_ratings) +
+				". Predictions: " + str(predictions_string))
 	except Error as e:
 		print(e)
 
---
+++ recommender_m1.py
@@ -0,0 +1,49 @@
+from recommender import Recommender
+import numpy as np
+import pandas as pd
+
+class MovieLensRecommender:
+
+    def __init__(self, factorised_movies, factorised_users, factorised_diag, movie_df):
+        """
+            Args:
+                factorised_movies: u x r matrix.
+                factorised_diag: r x 1 matrix.
+                movie_df: pandas DataFrame with u rows. Must have these columns: movierow, title, year. 
+                          May have other columns too. 
+        """
+        self.movie_df = movie_df
+        self.recommender = Recommender(U = factorised_movies,
+                                    S = factorised_diag)
+
+    def ratings_dict_to_column(self, x):
+        """Convert movie titles and ratings to column vector that can be passed to the recommender.
+            Args:
+                x: dict. Keys are movie titles, values are ratings.
+            Return: Vector with same length as self.recomender.U.
+        """
+        # Convert dict to data frame.
+        x_df = pd.DataFrame.from_dict(x, orient='index')
+        x_df = x_df.assign(Title=x_df.index, rating=x_df[0])
+        # Merge to get MovieRows.
+        x_df = x_df.merge(self.movie_df, on='title')
+        # Create column vector, with entries corresponding to MovieRows as appropriate ratings
+        # and nans elsewhere.
+        ratings_col = np.empty(len(self.movie_df))
+        ratings_col[:] = np.nan
+        ratings_col[list(x_df['movierow'] - 1)] = x_df['rating'] # MovieRow is 1-indexed.
+        return ratings_col
+
+    def get_predictions(self, new_user_ratings):
+        """
+            Args:
+                new_new_ratings: dict. Keys are titles, values are ratings.
+        """
+        # Make a prediction.
+        new_user_ratings_column = self.ratings_dict_to_column(new_user_ratings)
+        predictions = self.recommender.predict_for_new_v(new_user_ratings_column)
+        # predictions is a vector corresponding to MovieRow.
+        # Convert to title.
+        #TODO Make sure this returns a copy.
+        return self.movie_df.assign(prediction=predictions)
+		

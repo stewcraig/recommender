f3e8376ef6459b3dff9d34541c5fb056845b94f9
Andrew <apc53c@gmail.com>
2017-10-19 22:01:21 +0100

    Hard-coded diag for now as something wrong with diag table in database.

--- manage.py
+++ manage.py
@@ -17,12 +17,13 @@ cur = conn.cursor()
 movies_all_df = pd.read_sql("""SELECT * from movies""", conn)
 movies_df = movies_all_df.loc[:, ["movieid", "movierow", "title", "year"]]
 factorised_movies = movies_all_df.loc[:, ["latent1", "latent2", "latent3", "latent4", "latent5"]]
-factorised_diag = pd.read_sql("""SELECT * from diag""", conn)
-
+# Something wrong with diag table, so just hard-coding for now.
+# factorised_diag = pd.read_sql("""SELECT * from diag""", conn)
+diag = [9883.73108477146, 2604.8114716088617, 2046.7390225565089, 1836.1311790775487, 1428.3956754378983]
 # Set up recommender.
 print(factorised_diag.as_matrix())
 m = MovieLensRecommender(factorised_movies=factorised_movies.as_matrix(),
-                         factorised_diag=factorised_diag.as_matrix().tranpose(),
+                         factorised_diag=diag, # Should be: diag_df.as_matrix().transpose()
                          movie_df=movies_df)
 
 # Make some recommendations for a fixed new user.

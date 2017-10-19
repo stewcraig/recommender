90740e4c7aaaa4c5c5717ca32ee6aad3908af87a
Andrew <apc53c@gmail.com>
2017-10-19 22:03:22 +0100

    Deleted debug print.

--- manage.py
+++ manage.py
@@ -20,8 +20,8 @@ factorised_movies = movies_all_df.loc[:, ["latent1", "latent2", "latent3", "late
 # Something wrong with diag table, so just hard-coding for now.
 # factorised_diag = pd.read_sql("""SELECT * from diag""", conn)
 diag = [9883.73108477146, 2604.8114716088617, 2046.7390225565089, 1836.1311790775487, 1428.3956754378983]
+
 # Set up recommender.
-print(factorised_diag.as_matrix())
 m = MovieLensRecommender(factorised_movies=factorised_movies.as_matrix(),
                          factorised_diag=diag, # Should be: diag_df.as_matrix().transpose()
                          movie_df=movies_df)

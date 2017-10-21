4169ffe9950030d1a096cc49f3e791e3912ad36f
Andrew Craig <apc53c@gmail.com>
2017-10-21 09:55:32 +0100

    Fails gracefully if can't access database.

--- manage.py
+++ manage.py
@@ -9,27 +9,32 @@ from recommender_m1 import MovieLensRecommender
 
 # Get database connection.
 parse.uses_netloc.append("postgres")
-url = parse.urlparse(os.environ["DATABASE_URL"])
-conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
-cur = conn.cursor()
+try:
+  url = parse.urlparse(os.environ["DATABASE_URL"])
+  conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+  cur = conn.cursor()
 
-# Form a dataframe of movie information.
-movies_all_df = pd.read_sql("""SELECT * from movies""", conn)
-movies_df = movies_all_df.loc[:, ["movieid", "movierow", "title", "year"]]
-factorised_movies = movies_all_df.loc[:, ["latent1", "latent2", "latent3", "latent4", "latent5"]]
-# Something wrong with diag table, so just hard-coding for now.
-# factorised_diag = pd.read_sql("""SELECT * from diag""", conn)
-diag = [9883.73108477146, 2604.8114716088617, 2046.7390225565089, 1836.1311790775487, 1428.3956754378983]
+  # Form a dataframe of movie information.
+  movies_all_df = pd.read_sql("""SELECT * from movies""", conn)
+  movies_df = movies_all_df.loc[:, ["movieid", "movierow", "title", "year"]]
+  factorised_movies = movies_all_df.loc[:, ["latent1", "latent2", "latent3", "latent4", "latent5"]]
+  # Something wrong with diag table, so just hard-coding for now.
+  # factorised_diag = pd.read_sql("""SELECT * from diag""", conn)
+  diag = [9883.73108477146, 2604.8114716088617, 2046.7390225565089, 1836.1311790775487, 1428.3956754378983]
 
-# Set up recommender.
-m = MovieLensRecommender(factorised_movies=factorised_movies.as_matrix(),
-                         factorised_diag=diag, # Should be: diag_df.as_matrix().transpose()
-                         movie_df=movies_df)
+  # Set up recommender.
+  m = MovieLensRecommender(factorised_movies=factorised_movies.as_matrix(),
+                           factorised_diag=diag, # Should be: diag_df.as_matrix().transpose()
+                           movie_df=movies_df)
 
-# Make some recommendations for a fixed new user.
-new_user_ratings = {'Die Hard':5, 'Terminator':5, 'Speed':5}
-predictions_df = m.get_predictions(new_user_ratings)
-
+  # Make some recommendations for a fixed new user.
+  new_user_ratings = {'Die Hard':5, 'Terminator':5, 'Speed':5}
+  predictions_df = m.get_predictions(new_user_ratings)
+  predictions_string = predictions_df.sort_values('prediction', ascending=False).head()
+except KeyError:
+  new_user_ratings = {'Die Hard':5, 'Terminator':5, 'Speed':5}
+  predictions_string = "Running locally. Could not access database."
+  
 # Set up app.
 app = Flask(__name__)
 
@@ -42,7 +47,6 @@ def test_reply():
     try:
         value = request.args.get('value', "DEFAULT VALUE", type=str)
         return_value = value.upper()
-        predictions_string = predictions_df.sort_values('prediction', ascending=False).head()
         return jsonify(result = "JS said: " + value + " and Flask replied: " + return_value +
                 ".\n New ratings: " + str(new_user_ratings) +
                 ".\n Predictions: " + str(predictions_string))

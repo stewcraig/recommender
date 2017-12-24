875233199b21c6b19b7debbe6d5e8ce34b38d5ed
Andrew Craig <apc53c@gmail.com>
2017-12-24 21:50:42 +0000

    Now should use min and max years when making recommendations.

--- manage.py
+++ manage.py
@@ -14,24 +14,31 @@ try:
   url = parse.urlparse(os.environ["DATABASE_URL"])
   conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
   cur = conn.cursor()
-
-  # Form a dataframe of movie information.
   movies_all_df = pd.read_sql("""SELECT * from movies""", conn)
+  database_okay_flag = True
+except KeyError:
+  database_okay_flag = False
+
+if database_okay_flag:
+  # Form a dataframe of movie information.
   movies_df = movies_all_df.loc[:, ["movieid", "movierow", "title", "year"]]
   movie_titles = movies_df.title.tolist()
   factorised_movies = movies_all_df.loc[:, ["latent1", "latent2", "latent3", "latent4", "latent5"]]
   # Something wrong with diag table, so just hard-coding for now.
   # factorised_diag = pd.read_sql("""SELECT * from diag""", conn)
-  diag = [9883.73108477146, 2604.8114716088617, 2046.7390225565089, 1836.1311790775487, 1428.3956754378983]
+  diag = [9855.436, 2531.026, 2179.463, 1799.265, 1538.54]
 
   # Set up recommender.
   m = MovieLensRecommender(factorised_movies=factorised_movies.as_matrix(),
                            factorised_diag=diag, # Should be: diag_df.as_matrix().transpose()
                            movie_df=movies_df)
   # Set recommender function.
-  make_predictions = lambda new_user_ratings: m.get_predictions(new_user_ratings).sort_values('prediction', ascending=False).head()
-except KeyError:
-  # Probably couldn't access the database. Create a 'recommender' that always reports failure.
+  make_predictions = lambda new_user_ratings, year_min, year_max: (m.get_predictions(new_user_ratings)
+                                               .query("year >= " + str(year_min) + " and year <= " + str(year_max))
+                                               .sort_values('prediction', ascending=False)
+                                               .head())
+else:
+  # Database trouble. Create a 'recommender' that always reports failure.
   make_predictions = lambda x: "Could not access database."
   # Create an empty list of movie titles for autocompletion.
   movie_titles = ['aliens', 'speed']
@@ -48,11 +55,13 @@ def test_reply():
     try:
         movie_rating_string = request.args.get('value', "DEFAULT VALUE", type=str)
         print("Got: " + movie_rating_string)
-        # movie_rating_string is of form "movie1+rating1+movie2+rating2+".
-        # Convert to dict.
+        # movie_rating_string is of form "movie1+rating1+movie2+rating2+minYear+year+maxYear+year+".
+        # Convert to dict and extract years.
         parsed_movie_ratings = dict(toolz.itertoolz.partition(2, movie_rating_string.split("+")))
+        min_year = parsed_movie_ratings_and_years.pop('minYear') # Modifies dict in place.
+        max_year = parsed_movie_ratings_and_years.pop('maxYear') # Modifies dict in place.
         print("Parsed: " + str(parsed_movie_ratings))
-        predictions_string = str(make_predictions(parsed_movie_ratings)) 
+        predictions_string = str(make_predictions(parsed_movie_ratings, min_year, max_year)) 
         print("Predictions: " + str(predictions_string))
         jsonified = jsonify(result="Your ratings: " + str(parsed_movie_ratings) + "\n\n" +
                 	"Predictions: " + predictions_string)
